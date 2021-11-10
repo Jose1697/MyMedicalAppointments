1bd0cf02de3bff8788b4886dae856ba033a80994
Jose1697 <josenetboy@hotmail.com>
2021-11-09 21:55:53 -0500

    Clase anonimas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import model.Doctor;
-import model.Patient;
-import model.User;
+import model.*;
 
 import java.util.Date;
 
@@ -19,6 +17,24 @@ public class Main {
 
         User userPat = new Patient("Anahi","ana@ana.com");
         userPat.showDataUser();
+
+        User user1 = new User("Anahi","ana@ana.com") {
+            @Override
+            public void showDataUser() {
+                System.out.println("Doctor");
+                System.out.println("Hospital: Cruz Verde");
+                System.out.println("Departamento: Geriatria");
+            }
+        };
+        user1.showDataUser();
+
+        ISchedulable iSchedulable = new ISchedulable() {
+            @Override
+            public void schedule(Date date, String time) {
+
+            }
+        };
+        
         /*for (model.Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " " + aA.getTime());
 
